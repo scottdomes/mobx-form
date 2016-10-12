2b1ff0787755103c4415521eb1ca0df4115ba5c2
Stephen Shen <stephen@musefind.com>
2016-10-12 14:24:02 -0700

    added form support for fields that are nested within other elements

--- src/GenericForm.js
+++ src/GenericForm.js
@@ -84,8 +84,62 @@ export class GenericForm extends React.Component {
 
 }
 
+// When the labels are not direct children of the form we have to traverse through 
+// and find them
+class NDepthViewModelFormClass extends React.Component {
+   static propTypes = {
+    viewModel: React.PropTypes.object.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+  }
+
+  // Set the raw value on the view model on any changes. This avoids side effects
+  // by using a view model instead of the raw model.
+  handleChange(name, value) {
+    this.props.viewModel[name] = value
+  }
+
+  // Calls the onSave prop with the view model as an argument. Call submit() on
+  // the view model to commit the changes.
+  handleSave(e) {
+    e.preventDefault()
+    this.props.onSave(this.props.viewModel)
+  }
+
+  // Renders all the children with a value and an onChange handler. These should
+  // be used by the children to display the values.
+
+  render() {
+    return (
+      <form onSubmit={this.handleSave.bind(this)}>
+         {this.recursiveCloneChildren(this.props.children)}
+      </form>
+    )
+  }
+  
+  recursiveCloneChildren(children) {
+    return React.Children.map(children, child => {
+        var childProps = {};
+        if (React.isValidElement(child) && child.type.displayName === 'FieldClass') {
+            childProps = { 
+              onChange: this.handleChange.bind(this),
+              value: this.props.viewModel[child.props.name]};
+              
+        }
+        if (child.props) {
+            // String has no Prop
+            childProps.children = this.recursiveCloneChildren(child.props.children);
+            return React.cloneElement(child, childProps);
+        }
+        return child;
+    })
+  }
+
+}
+
+
 class FieldClass extends React.Component {
 
+  static displayName = 'FieldClass'
   static propTypes = {
     name: React.PropTypes.string.isRequired,
   }
